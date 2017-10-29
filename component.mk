cd534f89cf1facbbec499845510737e5fe8f0b18
Krishna Kumar A <krishnakumar.a@optimuslogic.in>
2017-10-29 21:00:42 +0530

    initial commit of ssd1306 lib (arduino-based) for use as esp-idf component

---
+++ component.mk
@@ -0,0 +1,11 @@
+# ssd1306 driver (from squix78) component makefile for esp-idf
+
+# includes
+COMPONENT_ADD_INCLUDEDIRS := .
+
+# source files
+COMPONENT_SRCDIRS := .
+
+# compiler flags for this component, c++
+#CXXFLAGS = -Wnoerror=return-type -Wnoerror=switch -Wnoerror=maybe-uninitialized
+
